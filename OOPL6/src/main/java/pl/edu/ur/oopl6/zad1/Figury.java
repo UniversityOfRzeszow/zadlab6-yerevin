df7ee2dba71a8ce980f600beaf6d5e7d90d54046
yerevin <skulskimat@gmail.com>
2018-04-14 18:45:43 +0200

    Rozwiazania zadan (2, fix decimal places) - 14.04.2018

--- OOPL6/src/main/java/pl/edu/ur/oopl6/zad1/Figury.java
+++ OOPL6/src/main/java/pl/edu/ur/oopl6/zad1/Figury.java
@@ -17,56 +17,56 @@ public class Figury {
      * Metoda statyczna obliczająca pole koła
      */
     public static double PoleKola(double r) {
-        return Math.round((Math.PI * Math.pow(r, 2)) * 1000) / 1000;
+        return Math.round((Math.PI * Math.pow(r, 2)) * 1000d) / 1000d;
     }
 
     /**
      * Metoda statyczna obliczająca obwód koła
      */
     public static double ObwodKola(double r) {
-        return Math.round((2 * Math.PI * r) * 1000) / 1000;
+        return Math.round((2 * Math.PI * r) * 1000d) / 1000d;
     }
 
     /**
      * Metoda statyczna obliczająca obwód koła
      */
     public static double poleKwadrat(double r) {
-        return Math.round(Math.pow(r, 2) * 1000) / 1000;
+        return Math.round(Math.pow(r, 2) * 1000d) / 1000d;
     }
 
     /**
      * Metoda statyczna obliczająca obwód koła
      */
     public static double obwodKwadrat(double r) {
-        return Math.round(4 * r * 1000) / 1000;
+        return Math.round(4 * r * 1000d) / 1000d;
     }
 
     /**
      * Metoda statyczna obliczająca obwód koła
      */
     public static double poleProstokat(double r, double b) {
-        return Math.round(r * b * 1000) / 1000;
+        return Math.round(r * b * 1000d) / 1000d;
     }
 
     /**
      * Metoda statyczna obliczająca obwód koła
      */
     public static double obwodProstokat(double r, double b) {
-        return Math.round((2 * r + 2 * b) * 1000) / 1000;
+        return Math.round((2 * r + 2 * b) * 1000d) / 1000d;
     }
 
     /**
      * Metoda statyczna obliczająca obwód koła
      */
     public static double poleStozka(double r, double l) {
-        return Math.round((Math.PI * r * r + Math.PI * r * l) * 1000) / 1000;
+        return Math.round((Math.PI * r * r + Math.PI * r * l) * 1000d) / 1000d;
     }
 
     /**
      * Metoda statyczna obliczająca obwód koła
      */
     public static double poleWalec(double r, double H) {
-        return Math.round((2 * Math.PI * r * r + 2 * Math.PI * r * H) * 1000) / 1000;
+        return Math.round((2 * Math.PI * r * r + 2 * Math.PI * r * H) * 1000d) / 1000d;
     }
 
 }
